049b6b928cfaaf8af6d027cea889b09d57c4f594
Sven Steudte <sven.steudte@gmail.com>
2017-05-31 01:01:00 +0200

    Added decoder software

---
+++ decoder/decoder.py
@@ -0,0 +1,94 @@
+#!/usr/bin/python
+
+import serial,os,re,datetime
+from subprocess import call
+import base91
+import binascii
+import urllib2
+import io
+import sys
+import argparse
+
+# Parse arguments from terminal
+parser = argparse.ArgumentParser(description='APRS/SSDV decoder')
+parser.add_argument('-c', '--call', help='Callsign of the station', required=True)
+parser.add_argument('-l', '--log', help='Name of the logfile')
+parser.add_argument('-n', '--grouping', help='Amount packets that will be sent to the SSDV server in one request', default=1, type=int)
+parser.add_argument('-d', '--device', help='Serial device (\'-\' for stdin)', default='-')
+parser.add_argument('-b', '--baudrate', help='Baudrate for serial device', default=9600, type=int)
+parser.add_argument('-s', '--server', help='Server URL', default='https://ssdv.habhub.org/api/v0/packets')
+args = parser.parse_args()
+
+if args.device is not '-': # Use serial connection (probably TNC)
+	try: 
+		serr = serial.Serial(
+			port=args.device,
+			baudrate=args.baudrate,
+		)
+	except:
+		sys.stderr.write('Error: Could not open serial port\n')
+		sys.exit(1)
+
+	ser = io.TextIOWrapper(io.BufferedRWPair(serr, serr, 1), newline = '\r', line_buffering = True) # Define Newline as \r
+
+
+# Open logging file
+if args.log is not None:
+	try:
+		f = open(args.log, 'a')
+	except:
+		sys.stderr.write('Error: Could not open logging file\n')
+		sys.exit(1)
+
+jsons = []
+
+while 1:
+	# Read a line (from stdin or serial)
+	data = sys.stdin.readline() if args.device is '-' else ser.readline()
+
+	# Parse line and detect data
+	m = re.search("(.*)\>APECAN:\{\{I(.*)", data)
+	try:
+		call = m.group(1)
+		aprs = m.group(2)
+	except:
+		continue # message format incorrect (probably no APRS message or line cut off too short)
+
+	if args.log is not None:
+		f.write(data) # Log data to file
+
+	data = base91.decode(aprs) # Decode Base91
+
+	if len(data) != 219:
+		continue # APRS message sampled too short
+
+	# Calculate CRC for SSDV server
+	crc = binascii.crc32(data) & 0xffffffff
+
+	# Create message for SSDV server (and save to array)
+	ssdv = '55' + binascii.hexlify(data) + ('%08x' % crc) + (64*'0')
+	jsons.append("""{
+		\"type\": \"packet\",
+		\"packet\": \"""" + ssdv + """\",
+		\"encoding\": \"hex\",
+		\"received\": \"""" + datetime.datetime.now().isoformat('T')[:19] + """Z\",
+		\"receiver\": \"""" + args.call + """\"
+	}""")
+
+	print 'Received packet call %02x%02x%02x%02x image %d packet %d' % (data[1], data[2], data[3], data[4], data[5], data[7] + data[6] * 256)
+
+	if len(jsons) >= args.grouping: # Enough packets collected, send them all to the server
+
+		req = urllib2.Request(args.server)
+		req.add_header('Content-Type', 'application/json')
+
+		json = "{\"type\":\"packets\",\"packets\":[" + ",".join(jsons) + "]}" # Group all SSDV packets into a big JSON
+		jsons = []
+
+		try:
+			result = urllib2.urlopen(req, "".join(json.split(' '))) # Send packets to server
+			print 'Send to SSDV data server: OK'
+		except urllib2.HTTPError, error: # The server did not like our packets :(
+			print 'Send to SSDV data server: failed'
+			print error.read()
+
